729678684e1dbea753b2354266c54da0f15c09ab
m3wo0o0o <leecjoseph15@gmail.com>
2025-05-07 21:21:17 +1000

    ughghgh

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let outputEL = document.getElementById("output-el")
 
-
 function getPos() {
 var x = document.getElementById("width").value;
 var y = document.getElementById("height").value;
-console.log(x * y)
-outputEL.textContent = "Output: " + (x * y)
+console.log(x / y)
+outputEL.textContent = "Aspect Ratio: " + (x / y) + ":1"
+}
+
+function methodChange() {
+
+    document.getElementById("method").value; 
+    console.log(document.getElementById("method").value);
 }
