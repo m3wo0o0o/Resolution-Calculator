dc2dd01440f8ac44e619d1e46493d6a6c7703be6
m3wo0o0o <leecjoseph15@gmail.com>
2025-05-08 15:55:41 +1000

    ugh

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let outputEL = document.getElementById("output-el")
+let listofOptions = [
+    "Width", "Height","Ratio"
+]
 
 function getPos() {
-var x = document.getElementById("width").value;
-var y = document.getElementById("height").value;
-console.log(x / y)
-outputEL.textContent = "Aspect Ratio: " + (x / y) + ":1"
+const x = document.getElementById("width").value;
+const y = document.getElementById("height").value;
+console.log(x / y);
+outputEL.textContent = "Aspect Ratio: " + (x / y) + ":1";
 }
 
 function methodChange() {
-
-    document.getElementById("method").value; 
-    console.log(document.getElementById("method").value);
+    let e = document.getElementById("method").value; 
+    //console.log(document.getElementById("method").value);
+    console.log(listofOptions[0]);
+    console.log(e);
+    document.getElementById("slot 1").textContent = e;
+    
 }
+
