fa4f263281472e62d60ca6dcfd984d537e99fb46
m3wo0o0o <leecjoseph15@gmail.com>
2025-05-08 18:17:53 +1000

    working version

--- index.js
+++ index.js
@@ -3,19 +3,41 @@ let listofOptions = [
     "Width", "Height","Ratio"
 ]
 
+
 function getPos() {
-const x = document.getElementById("width").value;
-const y = document.getElementById("height").value;
-console.log(x / y);
-outputEL.textContent = "Aspect Ratio: " + (x / y) + ":1";
+    let e = document.getElementById("method").value;
+    let i = document.getElementById("method").selectedIndex;
+    const x = document.getElementById("width").value;
+    const y = document.getElementById("height").value;
+    if (i == 0) {
+        outputEL.textContent = e + ": " + (x * y) + "px"
+    } else if (i == 1) {
+        outputEL.textContent = e + ": " + (x / y) + "px"   
+    } else {
+        outputEL.textContent = e + ": " + (x / y) + ":1";
+    }
+    console.log(x / y);
 }
 
 function methodChange() {
-    let e = document.getElementById("method").value; 
+    let e = document.getElementById("method").value;
+    let i = document.getElementById("method").selectedIndex;
+    console.log(i)
     //console.log(document.getElementById("method").value);
-    console.log(listofOptions[0]);
+    //console.log(listofOptions[0]);
     console.log(e);
-    document.getElementById("slot 1").textContent = e;
-    
+    //document.getElementById("slot 1").textContent = e;
+    //document.getElementById("slot 2").textContent = i;
+    outputEL.textContent = e + ": "
+    if (i == 0) {
+        document.getElementById("slot 1").textContent = listofOptions[1];
+        document.getElementById("slot 2").textContent = listofOptions[2];
+    } else if (i == 1){
+        document.getElementById("slot 1").textContent = listofOptions[0];
+        document.getElementById("slot 2").textContent = listofOptions[2];
+    } else {
+        document.getElementById("slot 1").textContent = listofOptions[0];
+        document.getElementById("slot 2").textContent = listofOptions[1];
+    }
 }
 
